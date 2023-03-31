214b19c17a8aa35a28b19e7a623b11974db7fee1
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-03-31 03:05:53 +0300

    change color loader "default" -> #303f9f

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
 import { ThreeDots } from 'react-loader-spinner';
 
 export const Loader = () => {
-	return <ThreeDots wrapperStyle={{ margin: "0 auto" }} />
+	return <ThreeDots color="#303f9f" wrapperStyle={{ margin: "0 auto" }} />
 }
